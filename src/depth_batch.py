117b0177a764d16c41120dbeb436d9f3747fcf68
Mary Yohannes <myohanne@broadinstitute.org>
2021-02-12 09:00:41 -0500

    update depth_batch.py

--- src/depth_batch.py
+++ src/depth_batch.py
@@ -1,26 +1,30 @@
 import hailtop.batch as hb
+import os 
 
 #function to run depth 
-def depth(cram, bed, label):
-	j = b.new_job(name=f'depth-{label}') #define job
+def depth(b, cram, bed, label):
+	j = b.new_job(name=f'depth-{label}') #define job and label it as "depth-<file_name>"
 	#use publically available Docker image that contains samtools  
 	j.image('hailgenetics/genetics:0.2.37')
-	g.cpu(4)
+	j.cpu(4)
 	#run samtools depth and save it to a tmp file
 	j.command(f'''samtools depth -b {bed} {cram} > {j.ofile}''')
 	return j
 
-# def merge(results):
-# 	g.command(f'''
-# 		python3 -c "
-# 		import hail as hl
-# 		ht = hl.import_table({results}, impute=True)
-# 		ht.write('')
-# 		"
-# 	''')
+#function to merge the results together 
+def merge(b, results):
+	k = b.new_job(name='merge_results')
+	if results:   
+		k.command(f''' 
+		python3 -c " \
+		import hail as hl \
+		{k.ofile} = hl.import_table({results}, impute=True) "
+		''')
+		return k
+		
 
 if __name__ == '__main__':
-	backend = hb.ServiceBackend(billing_project='daly-neale-sczmeta', bucket='MY_BUCKET') #set up backend 
+	backend = hb.ServiceBackend(billing_project='daly-neale-sczmeta', bucket='gs://imary116') #set up backend 
 
 	b = hb.Batch(backend=backend, name='coverage') #define batch
 
@@ -35,16 +39,16 @@ if __name__ == '__main__':
 	#run depth function on each cram file
 	results = []
 	for path in cram_file_paths:
-		label = os.path.splitext(os.path.basename(path))[0]
+		label = os.path.splitext(os.path.basename(path))[0] #only get the file name (without path and ext) 
 		cram = b.read_input(path) #input cram file
-		j = depth(cram, bed, label) #run depth 
+		j = depth(b, cram, bed, label) #run depth 
 		results.append(j.ofile)
 		# see what happens, but comment out after when you work on the merge command
-		b.write_output(j.ofile, 'gs://imary116/{label}.tsv') #write the output file in a google cloud bucket 
+		#b.write_output(j.ofile, 'gs://imary116/{label}.tsv') #write the output file in a google cloud bucket 
 
 	# merge command
-	# j = merge(cram, bed)
-	# b.write_output(j.ofile, 'gs://imary116/test-output.tsv')
+	m = merge(b, results)
+	b.write_output(m.ofile, 'gs://imary116/total_output.tsv')
 
 	b.run(open=True, wait=False) #run batch 
 
