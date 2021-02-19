a3aacac9348c9083bb641a44067f1585b78356f2
Mary Yohannes <myohanne@broadinstitute.org>
2021-02-19 09:03:56 -0500

    edit depth_batch.py script

--- src/depth_batch.py
+++ src/depth_batch.py
@@ -7,15 +7,15 @@ def depth(b, cram, bed, label):
 	#use publically available Docker image that contains samtools  
 	j.image('hailgenetics/genetics:0.2.37')
 	j.cpu(4)
-	#run samtools depth and save it to a tmp file
+	#run samtools depth on the .cram file and save it to a tmp file
 	j.command(f'''samtools depth -b {bed} {cram.cram} > {j.ofile}''')
 	return j
 
 #function to merge the results together 
 def merge(b, results):
 	k = b.new_job(name='merge_results')
-	if results:   
-		k.command(f''' 
+	if results:
+		k.command(f'''
 python3 -c "
 import hail as hl
 ht = hl.import_table({results}, impute=True)
@@ -31,30 +31,44 @@ if __name__ == '__main__':
 	#read in the bed file that has the coverage regions for each chr
 	bed = b.read_input('gs://imary116/coverage_region.bed')
 	
-	#paths to the cram files on google cloud 
-	root = 'gs://fc-7a86ab95-f5c5-4ac1-976b-e57dbc601eb1/sc_global_japan_rik_Huang_Yoshikawa_schizophrenia_exome/RP-1855/Exome'
-	subpath = '**/**/*.cram'
-	cram_file_paths = subprocess.check_output(['gsutil', 'ls', f'{root}/{subpath}']).decode().split('\n')
-	cram_file_paths = cram_file_paths[0:3]
+	#set up paths to the cram files on google cloud
+	###root = 'gs://fc-7a86ab95-f5c5-4ac1-976b-e57dbc601eb1/sc_global_japan_rik_Huang_Yoshikawa_schizophrenia_exome/RP-1855/Exome'
+	###subpath = '**/**/*.cram'
+
+	#subprocess.check_output() runs what is inside () as if it was being run in the terminal, decode() sets up the output for split() on a new line
+	# returns a list of file paths: ex. ['gs://.../JP-RIK-C-00070.cram', 'gs://.../JP-RIK-C-00071.cram', ... ]
+	###cram_file_paths = subprocess.check_output(['gsutil', 'ls', f'{root}/{subpath}']).decode().split('\n')
+
+
+	###cram_file_paths = cram_file_paths[0:3] # the first three file paths in the list
 
 	cram_file_paths = [
 		'gs://imary116/JP-RIK-C-00070.cram',
 		'gs://imary116/JP-RIK-C-00071.cram']
 	
 	#run depth function on each cram file
-	results = []
+	results = [] #empty lists for the depth function outputs
 	for path in cram_file_paths:
-		label = os.path.splitext(os.path.basename(path))[0] #only get the file name (without path and ext) 
-		root = os.path.splitext(path)[0]
-		# read in multiple inputs
+
+		# only get the file name - without path (removed by os.path.basename) and '.cram' ext (removed by os.path.splitext)
+		label = os.path.splitext(os.path.basename(path))[0] # ex output: JP-RIK-C-00070
+		##root = os.path.splitext(path)[0] #obtain path with file name (without the '.cram' extension) - ex output: 'gs://... /JP-RIK-C-00070'
+
+
+		# read in multiple inputs (need both the .cram and .crai (index) files for samtools depth to run faster)
+		# cram = b.read_input_group(
+		# 	cram=f'{root}.cram',
+		# 	crai=f'{root}.crai')
 		cram = b.read_input_group(
-			cram=f'{root}.cram',
-			crai=f'{root}.crai')
+			cram=path,
+			crai=f'{path}.crai')
+
 		# cram = b.read_input(path) #input cram file
-		j = depth(b, cram, bed, label) #run depth 
-		results.append(j.ofile)
-		# see what happens, but comment out after when you work on the merge command
-		b.write_output(j.ofile, f'gs://imary116/data/test-coverage/{label}.tsv') #write the output file in a google cloud bucket 
+		j = depth(b, cram, bed, label) #run depth function
+		results.append(j.ofile) #append outputs to the list 'results'
+
+		# see what happens, but comment out after when you work on the merge command - took an hour to run
+		#b.write_output(j.ofile, f'gs://imary116/data/test-coverage/{label}.tsv') #write the output file in a google cloud bucket
 
 	# merge command
 	m = merge(b, results)
